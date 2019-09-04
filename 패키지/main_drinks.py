c5cc1749dd4f7ac0c92644a441d0c9689d3012e4
Chaelin <wonchaelin6@gmail.com>
2019-09-05 08:59:35 +0900

    foods.drinks 활용

---
+++ 패키지/main_drinks.py
@@ -0,0 +1,13 @@
+#import에 있는 거는 무조건 써주깅 ~ 
+
+ # from foods.drinks import milk,cola,water 귀찮으니까 한줄에다가 ㄱ
+from foods.drinks import *
+
+# from foods.drinks import cola
+# from foods.cola import cola
+
+
+milk.drink()
+cola.drink()
+water.drink()
+
