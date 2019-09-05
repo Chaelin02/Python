58d1ced5eb710a61319be175f1d2ebc81ee33bc4
Chaelin <wonchaelin6@gmail.com>
2019-09-05 09:09:54 +0900

    foods.friuts 활용

---
+++ 패키지/main_friuts.py
@@ -0,0 +1,9 @@
+
+# from foods.fruits import grape,peach,watermelon
+from foods.fruits import *
+
+grape.eat()     # 톡 터지는 맛으로 먹는 포도!
+peach.eat()     # 달콤새콤 맛있다 ~
+watermelon.eat() # 달다구리하면서 계속 땡기는 이시리는 맛!!
+
+
