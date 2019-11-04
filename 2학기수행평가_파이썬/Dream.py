99d87623f088347a7fbca8c593a704723407f93e
Chaelin <wonchaelin6@gmail.com>
2019-11-04 10:09:33 +0900

    TicTacTo GameManager_콘솔, GUI

--- 2학기수행평가_파이썬/Dream.py
+++ 2학기수행평가_파이썬/Dream.py
@@ -1,4 +1,11 @@
 class Dream
 
     self 'title'
-    self
+    self todo1
+    self todo2
+    self todo3
+
+    난 지금 객체로 해서 안되는거임
+    텍스트로 넣으면 가능함
+Dream 목표라는 클래스를 만들어서
+목ㅎ표 3가지랑 연결 시키기
--- IX. 프로젝트/game_manager.py
+++ IX. 프로젝트/game_manager.py
@@ -1,6 +1,7 @@
-
 from tictactoe import TicTacToe
-
+import tkinter
+from tkinter import messagebox
+import math
 
 class GameManager:
     def __init__(self):
@@ -27,9 +28,70 @@ class GameManager:
                 print("무승부")
                 break
 
+class GameManager_GUI:
+    def __init__(self):
+        self.ttt = TicTacToe()
+        CANVAS_SIZE = 300
+        self.TITLE_SIZE = CANVAS_SIZE / 3
+
+        self.root = tkinter.Tk()
+        self.root.title("틱택토")
+        self.root.geometry(str(CANVAS_SIZE)+"x"+str(CANVAS_SIZE))  #창 크기  300x300 으로 표시한거임
+        self.root.resizable(width=False, height=False)   #창크기 변경 ㄴ ㄴ
+        self.canvas = tkinter.Canvas(self.root, bg="white", width=CANVAS_SIZE, height=CANVAS_SIZE)
+        self.canvas.pack()
+
+        self.images = dict()
+        self.images["O"] = tkinter.PhotoImage(file="img/O.gif")
+        self.images["X"] = tkinter.PhotoImage(file="img/X.gif")
+
+        self.canvas.bind("<Button-1>",self.click_handler)
+
+    def click_handler(self,event):
+        row = int(event.y//self.TITLE_SIZE)      #math.floor(event.y/self.TITLE_SIZE)
+        col = int(event.x//self.TITLE_SIZE)
+        print(row,col)
+        self.ttt.set(row,col)
+       # print(self.ttt)
+        #draw_board()
+        self.draw_board()
+
+
+        #check_winner()
+
+        if self.ttt.check_winner() == "O":
+            messagebox.showinfo("게임오버","O win!!!!!")
+            self.root.quit()
+        elif self.ttt.check_winner()=="X":
+            messagebox.showinfo("게임오버","X win!!!!")
+            self.root.quit()
+        elif self.ttt.check_winner() == "d":
+            messagebox.showinfo("게임오버","무승부")
+            self.root.quit()
+
+    def draw_board(self):
+        #clear
+        self.canvas.delete("all")
+
+        x = 0
+        y = 0
+        for i, v in enumerate(self.ttt.board):
+            if v == ".":
+                pass
+            elif v == "O":
+                self.canvas.create_image(x,y,anchor="nw",image = self.images["O"])
+            elif v == "X":
+                self.canvas.create_image(x, y, anchor="nw", image = self.images["X"])
+            x += self.TITLE_SIZE
+            if i % 3 == 2:
+                x = 0
+                y += self.TITLE_SIZE
+
+    def play(self):
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
-    gm = GameManager()
+    gm = GameManager_GUI()
     gm.play()
 
