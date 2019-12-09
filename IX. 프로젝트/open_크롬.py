0b9fddb0f6268303cb3b0ffef4da5a5b34da8928
Chaelin <wonchaelin6@gmail.com>
2019-12-09 09:55:10 +0900

    pyautogui- 여러번 인식하자

--- IX. 프로젝트/open_크롬.py
+++ IX. 프로젝트/open_크롬.py
@@ -3,12 +3,16 @@ import time
 
 if __name__ == '__main__':
     #크롬이미지 인식하자
-    data = pag.locateOnScreen("crome_icon.PNG")
-    print(data)
+    data = pag.locateCenterOnScreen("crome_icon.PNG")
+    #  print(data)
     # 가운데 좌표 찾자
     #center = (data.left+(data.width/2),(data.height/2))
-    center = pag.center(data)
+    # center = pag.center(data)
     # 마우스 이동
-    pag.moveTo(center,duration=2)
+    #pag.moveTo(center,duration=2)
     # 더블클림하자
-    pag.click()
+    #pag.click()
+    for data in datas:
+        print(data)
+        center = pag.center(data)
+        pag.moveTo(center,duration=1)
